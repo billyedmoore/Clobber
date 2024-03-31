fba1a6be0fcaf8d2c178e9c97d173b59b259d667
Billy Moore <billyedmoore@yahoo.com>
2024-03-31 23:00:45 +0100

    Reordered "src/header.h" to make it more logical (slightly).

--- src/header.h
+++ src/header.h
@@ -19,13 +19,10 @@
 #define FONT_CYAN "\e[1;96m"
 #define FONT_GREEN "\e[0;92m"
 
-extern pid_t background_processes[MAX_BACKGROUND];
-extern int number_alive_background_processes;
-extern pid_t foreground_process;
-
+// DEFINING A COMMAND
+//
+//
 enum redirection_types { NONE, NORMAL, APPEND };
-
-enum operating_mode { INTERACTIVE, SCRIPT };
 
 // Structure to store a command.
 struct command {
@@ -35,40 +32,44 @@ struct command {
   char *redirection_file;
   enum redirection_types redirection_type;
 };
-
 typedef struct command Command;
+int run_command(Command cmd);
+
+// THE STATE OF THE PROGRAM
+//
+extern pid_t background_processes[MAX_BACKGROUND];
+extern int number_alive_background_processes;
+extern pid_t foreground_process;
+enum operating_mode { INTERACTIVE, SCRIPT };
 
 // Stucture to store a built in function.
+typedef struct built_in_func_t built_in_func;
 struct built_in_func_t {
   int (*func)(Command cmd);
   char *name;
 };
 
-typedef struct built_in_func_t built_in_func;
-
-extern built_in_func *builtins;
-built_in_func *create_builtins();
-
+// Structure to store a list of commands
+typedef struct command_list_t command_list;
 struct command_list_t {
   size_t len;
   Command *commands;
   size_t allocated_len;
 };
-
-typedef struct command_list_t command_list;
-
-int main_loop();
-int run_command(Command cmd);
-void free_before_exit();
+extern command_list command_queue;
 command_list create_command_queue();
 void delete_command_list(command_list cmd_lst);
+void populate_command_queue();
 Command get_next_command_from_queue();
 
-extern command_list command_queue;
+extern built_in_func *builtins;
+built_in_func *create_builtins();
 
+int main_loop();
 char *prompt();
+void free_before_exit();
+
 Command parse_command(char *line);
-void populate_command_queue();
 void delete_command(Command cmd);
 char **copy_string_array(char **source, int num_elements);
 
